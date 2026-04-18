348633f9f22400deae3e61447cfedd951d19dd33
oleiarme <96879215+oleiarme@users.noreply.github.com>
2026-04-19 00:55:44 +0100

    Update replication configuration for db_password secret

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,7 +10,13 @@ resource "google_service_account" "vm_sa" {
 
 resource "google_secret_manager_secret" "db_password" {
   secret_id = "db-password"
-  replication { auto {} }
+  replication {
+    user_managed {
+      replicas {
+        location = "us-central1"
+      }
+    }
+  }
 }
 
 resource "google_secret_manager_secret_version" "db_password_v" {
