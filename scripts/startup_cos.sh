279d7c8cc4cca4a3511d608fd33aeb7b435e670f
oleiarme <96879215+oleiarme@users.noreply.github.com>
2026-05-04 18:13:15 +0100

    fix font

--- scripts/startup_cos.sh
+++ scripts/startup_cos.sh
@@ -658,9 +658,9 @@ docker run -d \
   -v /mnt/disks/data/n8n:/home/node/.n8n \
   -e DB_TYPE=postgresdb \
   -e DB_POSTGRESDB_HOST=postgres \
-  -e DB_POSTGRESDB_PORT="${DB_PORT:-5432}" \
-  -e DB_POSTGRESDB_DATABASE="${DB_NAME:-postgres}" \
-  -e DB_POSTGRESDB_USER="${DB_USER:-n8n}" \
+  -e DB_POSTGRESDB_PORT="${db_port:-5432}" \
+  -e DB_POSTGRESDB_DATABASE="${db_name:-postgres}" \
+  -e DB_POSTGRESDB_USER="${db_user:-n8n}" \
   -e DB_POSTGRESDB_PASSWORD="$(cat /dev/shm/n8n-secrets/db_password)" \
   -e N8N_ENCRYPTION_KEY="$(cat /dev/shm/n8n-secrets/n8n_key)" \
   -e N8N_EXECUTIONS_MODE=queue \
@@ -688,9 +688,9 @@ docker run -d \
   -v /mnt/disks/data/n8n:/home/node/.n8n \
   -e DB_TYPE=postgresdb \
   -e DB_POSTGRESDB_HOST=postgres \
-  -e DB_POSTGRESDB_PORT="${DB_PORT}" \
-  -e DB_POSTGRESDB_DATABASE="${DB_NAME}" \
-  -e DB_POSTGRESDB_USER="${DB_USER}" \
+  -e DB_POSTGRESDB_PORT="${db_port}" \
+  -e DB_POSTGRESDB_DATABASE="${db_name}" \
+  -e DB_POSTGRESDB_USER="${db_user}" \
   -e DB_POSTGRESDB_PASSWORD="$(cat /dev/shm/n8n-secrets/db_password)" \
   -e N8N_ENCRYPTION_KEY="$(cat /dev/shm/n8n-secrets/n8n_key)" \
   -e N8N_EXECUTIONS_MODE=queue \
